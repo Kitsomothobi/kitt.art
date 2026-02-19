20b419e283f2275dc112a7cdd7b19d15d7b7916f
Kitso Mothobi <kitsomothobi@gmail.com>
2026-02-19 15:09:16 +0200

    Added a base template layout and a proto-navbar

--- app/main.py
+++ app/main.py
@@ -2,14 +2,16 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from datetime import datetime
 
 # Initliaze the fastapi obeject
 app = FastAPI()
 
+# Conencting Jinja to our html/css files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
-
+# Homepage decorater
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "year": datetime.now().year})
