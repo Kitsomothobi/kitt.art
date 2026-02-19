3f4d1286e02745716a77b06cd2f3aa1cf95de6f2
Kitso Mothobi <kitsomothobi@gmail.com>
2026-02-19 17:43:52 +0200

    Added the html pages to the project, alognside other CSS implementations

--- app/main.py
+++ app/main.py
@@ -11,7 +11,40 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
-# Homepage decorater
+# --------------
+def render(request: Request, template: str, active_page: str, **context):
+    base = {
+        "request": request,
+        "year": datetime.now().year,
+        "active_page": active_page
+        }
+    # Whenever a new webpage is clicked, we need to immediately update the context for our css
+    base.update(context)
+    return templates.TemplateResponse(template, base)
+
+
+# Page routes
+    #1 - Homepage decorater
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "year": datetime.now().year})
+    return render(request, "index.html", "home")
+
+    #2 - Gallery page
+@app.get("/gallery")
+def gallery(request: Request):
+    return render(request, "gallery.html", "gallery")
+
+    #3 - Blog page
+@app.get("/blog")
+def blog(request: Request):
+    return render(request, "blog.html", "blog")
+
+    #4 - Commissions
+@app.get("/commissions")
+def commissions(request: Request):
+    return render(request, "commissions.html", "commissions")
+
+    #5 - Projects
+@app.get("/projects")
+def projects(request: Request):
+    return render(request, "projects.html", "projects")
